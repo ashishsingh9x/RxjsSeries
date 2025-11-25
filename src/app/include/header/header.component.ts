2b457d4cdef111ffd26c67b1106cc5d7a4140921
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-26 00:21:12 +0530

    Added bootstrap in scss and design in header component to show clickable promise and observable menu

--- src/app/include/header/header.component.ts
+++ src/app/include/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  navOpen: boolean = false;
+  onNavToggle() {
+    this.navOpen = !this.navOpen;
+  }
 
+  closeNav() {
+    this.navOpen = false;
+  }
 }
