0e73281de5c93fc535ae20bb295e2a49bcf4cf22
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-25 11:49:53 +0530

    Added header and promise new component

---
+++ src/app/promise/promise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  imports: [],
+  templateUrl: './promise.component.html',
+  styleUrl: './promise.component.scss',
+})
+export class PromiseComponent {
+
+}
