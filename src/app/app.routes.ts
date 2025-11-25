3c3ca89be631cccc87edff694938bd4436c9bc18
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-26 00:22:03 +0530

    Promise example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+import { ObservableComponent } from './observable/observable.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'promise', component: PromiseComponent},
+    { path: 'observable', component: ObservableComponent},
+];
--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-promise',
@@ -6,6 +6,66 @@ import { Component } from '@angular/core';
   templateUrl: './promise.component.html',
   styleUrl: './promise.component.scss',
 })
-export class PromiseComponent {
+export class PromiseComponent implements OnInit  {
 
+  promiseVal: any;
+  ngOnInit(): void {
+    // let buyLaptop = new Promise(function (resolve, reject) {
+    //   // resolve('Promise is resolved')
+    //   reject('Promise is reject')
+    // });
+
+    let buyLaptop = new Promise((resolve, reject) => {
+       //resolve('Promise is resolved')
+       //reject('Promise is reject')
+      
+       
+       if (this.DellAvailable()) {
+          return setTimeout(() => {
+            resolve(this.dell)
+          }, 3000)
+        }
+        else if (this.HpAvailable()) {
+          return setTimeout(() => {
+            resolve(this.hp)
+          }, 3000)
+        }
+        else {
+          return setTimeout(() => {
+            reject(this.notAvai)
+          }, 3000)
+        }
+    });
+
+    buyLaptop.then(res => {
+      console.log('Success =>', res);
+    }).catch(err => {
+      console.log('Error =>', err);
+    })
+  }
+
+  DellAvailable() {
+    return false;
+  }
+
+  HpAvailable() {
+    return false;
+  }
+
+  dell = {
+    brand: 'Dell',
+    hardDisk: '2 TB',
+    color: 'Black'
+  }
+
+  hp = {
+    brand: 'HP',
+    hardDisk: '1 TB',
+    color: 'Silver'
+  }
+
+  notAvai = {
+    brand: 'Not Available',
+    status: 'Failed'
+  }
 }
