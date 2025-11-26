eb1f181a089ef5c524e069702a6f9e60a0b560a8
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-26 13:42:21 +0530

    Created interval component and mapped it's child rounting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PromiseComponent } from './promise/promise.component';
 import { ObservableComponent } from './observable/observable.component';
 import { ListComponent } from './observable/list/list.component';
 import { FromEventComponent } from './observable/from-event/from-event.component';
+import { IntervalComponent } from './observable/interval/interval.component';
 
 export const routes: Routes = [
     { path: 'promise', component: PromiseComponent},
     { path: 'observable', component: ObservableComponent, children: [
         { path: '', component: ListComponent },
-        { path: 'fromEvent', component: FromEventComponent }
+        { path: 'fromEvent', component: FromEventComponent },
+        { path: 'interval', component: IntervalComponent }
     ]},
 ];
---
+++ src/app/observable/interval/interval.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interval',
+  imports: [],
+  templateUrl: './interval.component.html',
+  styleUrl: './interval.component.scss',
+})
+export class IntervalComponent {
+
+}
