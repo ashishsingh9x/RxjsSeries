7d80c66e9e5d5585f9236d4eb72e05780849b75e
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-26 14:28:22 +0530

    interval operator

--- src/app/observable/interval/interval.component.ts
+++ src/app/observable/interval/interval.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { delay, interval, Subscription, timer } from 'rxjs';
+import { DesignUtilityService } from '../../services/design-utility.service';
 
 @Component({
   selector: 'app-interval',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './interval.component.html',
   styleUrl: './interval.component.scss',
 })
-export class IntervalComponent {
+export class IntervalComponent implements OnInit {
+
+  constructor(private service: DesignUtilityService) { }
+  obsMessage: any;
+  videoSubscription!: Subscription;
+
+  ngOnInit(): void {
+    const broadCastVideos = interval(1000);
+    
+    this.videoSubscription = broadCastVideos.subscribe(res => {
+      console.log(res);
+      this.obsMessage = 'Videos ' + res;
+      this.service.print(this.obsMessage);
+
+      if (res >= 5) {
+        this.videoSubscription.unsubscribe(); 
+      }
+    })
+  }
 
 }
