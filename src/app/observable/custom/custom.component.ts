327ab4ace69396458cc3141516f81a7bc0ac96e5
Ashish Singh <ashishsingh.9x@gmail.com>
2026-01-04 19:57:34 +0530

    Custom observable - Custom Intervals

--- src/app/observable/custom/custom.component.ts
+++ src/app/observable/custom/custom.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { count, Observable, Subscription } from 'rxjs';
 import { DesignUtilityService } from '../../services/design-utility.service';
 import { CommonModule } from '@angular/common';
 
@@ -10,9 +10,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './custom.component.html',
   styleUrl: './custom.component.scss',
 })
-export class CustomComponent implements OnInit {
+export class CustomComponent implements OnInit, OnDestroy {
 
   techStatus: string = '';
+  techStatus2: string = '';
+  subs2?: Subscription;
+  constructor(private service: DesignUtilityService) {
+    
+  }
+
+  ngOnDestroy(): void {
+    this.subs2?.unsubscribe();
+  }
+
   ngOnInit(): void {
     // Example 01: (Manual)
     const cusObs1 = new Observable(observer => {
@@ -31,7 +41,7 @@ export class CustomComponent implements OnInit {
       }, 4000);
       setTimeout(() => {
         observer.next('JQuery');
-        observer.complete();
+        //observer.complete();
         this.techStatus =  'completed';
       }, 5000);
     })
@@ -48,10 +58,36 @@ export class CustomComponent implements OnInit {
         this.techStatus = 'completed';
       }
     });
-  }
 
-  constructor(private service: DesignUtilityService) {
-    
-  }
+    // Example 2 - Custom Intervals
+    const Arr2 = ['Angular', 'Javascript', 'Html', 'Css', 'Typescript']
+    const cusObs2 = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(Arr2[count]);
 
+        if (count >=3) {
+          observer.error('Error Emit');
+        }
+
+        if (count >= 5) {
+          observer.complete();
+        }
+        count++;
+      }, 1000)
+    })
+
+    this.subs2 = cusObs2.subscribe({
+      next: (res) => {
+        this.service.print(res, 'elContainer2');
+      },
+      error: (err) => {
+        console.log('Error emit')
+        this.techStatus2 = 'error';
+      },
+      complete: () => {
+        this.techStatus2 = 'completed';
+      }
+    })
+  }
 }
